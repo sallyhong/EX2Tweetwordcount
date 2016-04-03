9adb3d5dd5d7d930ed5cb28d1877de650837c1e3
Sally Hong <sally.hong@gmail.com>
2016-04-03 22:52:12 +0900

    Update drop_create_table.py

--- drop_create_table.py
+++ drop_create_table.py
@@ -3,8 +3,9 @@ import psycopg2
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
 cur = conn.cursor()
 
-cur.execute("drop table tweetwordcount;")
+cur.execute("drop table if exists tweetwordcount;")
 conn.commit()
 
-cur.execute("CREATE TABLE Tweetwordcount (word TEXT NOT NULL PRIMARY KEY, count INT NOT NULL);")
+# The optoin 'if not exists' is unavailable in postgres 8.4. Use 9.0+ to use this option or take out.
+cur.execute("CREATE TABLE if not exists Tweetwordcount (word TEXT NOT NULL PRIMARY KEY, count INT NOT NULL);")
 conn.commit()
